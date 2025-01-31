4d533a8b89487c64dd4b110ab363a3d4c4c65ac4
mindex <dev@mindex.com>
2025-01-30 21:27:03 -0500

    Removed unused import

--- src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
+++ src/test/java/com/mindex/challenge/ChallengeApplicationTests.java
@@ -1,6 +1,5 @@
 package com.mindex.challenge;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
